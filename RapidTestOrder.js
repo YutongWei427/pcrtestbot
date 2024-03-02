661d5d92b41caa2db3dcd8d756a394245da2ffb9
YutongWei427 <w979390059@gmail.com>
2024-03-01 23:10:57 -0500

    add menu

--- RapidTestOrder.js
+++ RapidTestOrder.js
@@ -3,57 +3,79 @@ class RapidTestOrder {
     this.OrderState = {
       WELCOMING: () => {
         let aReturn = [];
-        this.stateCur = this.OrderState.RESERVING;//首次进入后覆盖stateCur  再次进入时就不会触发欢迎语
-        aReturn.push("Welcome to Yutong's Restaurant.");
-        aReturn.push("Would you like to reserve a pizza?");//第一次询问是否需要汉堡包
+        this.stateCur = this.OrderState.Hambuger;
+        aReturn.push("Welcome to Best restaurant in Brantford.");
+        aReturn.push("Would you like to reserve a hambuger?");
         return aReturn;
       },
-      RESERVING: (sInput) => {
+      Hambuger: (sInput) => {
         let aReturn = [];
         if (sInput.toLowerCase().startsWith('yes')) {
-          if(this.type=="large"||this.type=="small"){
-            aReturn.push("Do you need coke?");//需要番茄酱后询问是否需要可乐
-          }else if(this.type=="coke"){//需要可乐  输出消息并设置isdone为结束
+        this.stateCur = this.OrderState.HambugerSize;
+          aReturn.push("Large or small?");
+        }else if(sInput.toLowerCase().startsWith('no')){
+        this.stateCur = this.OrderState.Pizza;
+          aReturn.push("Would you like to reserve a Pizza?");
+        }
+        return aReturn;
+      },
+      Pizza:(sInput)=>{
+        let aReturn = [];
+        if (sInput.toLowerCase().startsWith('yes')) {
+        this.stateCur = this.OrderState.HambugerSize;
+          aReturn.push("Large or small?");
+        }else if(sInput.toLowerCase().startsWith('no')){
+          this.stateCur = this.OrderState.Pizza;
+          aReturn.push("Thanks for trying our reservation system");
+          aReturn.push("Maybe next time")
+        }
+        return aReturn;
+      },
+      HambugerSize: (sInput) => {
+        let aReturn = [];
+        this.stateCur = this.OrderState.Ketchup;
+        if (sInput.toLowerCase().startsWith('large')||sInput.toLowerCase().startsWith('small')) {
+          aReturn.push("Do you need Ketchup?");
+        }
+        return aReturn;
+      },
+      Ketchup:(sInput)=>{
+        let aReturn = [];
+        this.stateCur = this.OrderState.COKE;
+        if (sInput.toLowerCase().startsWith('yes')||sInput.toLowerCase().startsWith('no')) {
+          aReturn.push("Do you also want to add a coke?");
+        }
+        return aReturn;
+      },
+      COKE:(sInput)=>{
+        let aReturn = [];
+        this.stateCur = this.OrderState.RESERVING;
+        if (sInput.toLowerCase().startsWith('yes')||sInput.toLowerCase().startsWith('no')) {
+          aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          let d = new Date();
+          d.setMinutes(d.getMinutes() + 120);
+          aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+        }
+        return aReturn;
+      },
+
+      RESERVING: (sInput) => {
+        let aReturn = [];
+        if (sInput.toLowerCase().startsWith('yes')||sInput.toLowerCase().startsWith('no')) {
           aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
           let d = new Date();
           d.setMinutes(d.getMinutes() + 120);
           aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
           this.isDone = true;
-          }else if(this.type=="first"||this.type=="second"){
-            aReturn.push("Large or small?");//确定需要汉堡或者披萨后询问大小
-          }
-        } else if (sInput.toLowerCase().startsWith('large')) {
-          aReturn.push("Do you need ketchup?");//确定需要大后询问是否需要番茄酱
-        }else if (sInput.toLowerCase().startsWith('small')) {
-          aReturn.push("Do you need ketchup?");//确定需要小后询问是否需要番茄酱
-        }
-        else if (sInput.toLowerCase().startsWith('no')) {
-          if(this.type=="large"||this.type=="small"){//不要番茄酱后询问是否需要可乐
-            aReturn.push("Do you need coke?");
-          }else if(this.type=="cola"){//不需要可乐  输出订单消息并设置isdone为结束
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
-            let d = new Date();
-            d.setMinutes(d.getMinutes() + 120);
-            aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
-            this.isDone = true;
-            }else if(this.type=="first"){
-              aReturn.push("Would you like to reserve a hamburger?");//拒绝汉堡后询问是否需要披萨
-            }else{
-              aReturn.push("Thanks for trying our restaurant");
-              aReturn.push("Maybe next time")//什么都不吃  撒由那拉
-              this.isDone = true;
-            }
         }
         return aReturn;
       }
     };
-    //初始化给stateCur赋值欢迎功能
     this.stateCur = this.OrderState.WELCOMING;
     this.isDone = false;
     this.sFrom = sFrom;
   }
-  handleInput(sInput,type) {
-    type?this.type=type:null
+  handleInput(sInput) {
     return this.stateCur(sInput);
   }
   isDone() {
--- spec/RapidTestOrderSpec.js
+++ spec/RapidTestOrderSpec.js
@@ -11,11 +11,26 @@ describe("order test", function(){
         oOrder.handleInput("hello");
         const aResult = oOrder.handleInput("yes");
         expect(true).toEqual(aResult[0].toLowerCase().includes("reserved"));
+        expect(true).toEqual(aResult[1].toLowerCase().includes("Ketchup"));
+        expect(true).toEqual(aResult[1].toLowerCase().includes("coke"));
     });
     it("tests results of no", function(){
         const oOrder = new RapidTestOrder("519-123-4567");
         oOrder.handleInput("hello");
         const aResult = oOrder.handleInput("no");
+        expect(true).toEqual(aResult[1].toLowerCase().includes("maybe next time"));
+        expect(true).toEqual(aResult[1].toLowerCase().includes("coke"));
+    });
+    it("tests results of large", function(){
+        const oOrder = new RapidTestOrder("519-123-4567");
+        oOrder.handleInput("hello");
+        const aResult = oOrder.handleInput("large");
+        expect(true).toEqual(aResult[1].toLowerCase().includes("maybe next time"));
+    });
+    it("tests results of small", function(){
+        const oOrder = new RapidTestOrder("519-123-4567");
+        oOrder.handleInput("hello");
+        const aResult = oOrder.handleInput("small");
         expect(true).toEqual(aResult[1].toLowerCase().includes("maybe next time"));
     });
 })
